1aae4e6eb62733234f3537defb4863c57b291dea
allen0558 <liuxuan0558@qq.com>
2013-04-02 16:39:44 +0800

    试图转换
    正视图、俯视图、左视图的动画显示

--- Assets/New Scripts/ControlPanel_Script/ViewSelectModule.cs
+++ Assets/New Scripts/ControlPanel_Script/ViewSelectModule.cs
@@ -5,26 +5,35 @@ using UnityEngine;
 using System.Collections;
 
 public class ViewSelectModule : MonoBehaviour {
-	GameObject mainObject;//内容--用于获得机床的基准坐标点，姓名--刘旋，时间--2013-4-1
+	//GameObject mainObject;//内容--用于获得机床的基准坐标点，姓名--刘旋，时间--2013-4-1
 	GameObject mainCamera;//内容--用于对摄像机进行设置，姓名--刘旋，时间--2013-4-1
-	float x_distance;//内容--x轴方向上摄像机偏离机床基准点的距离，姓名--刘旋，时间--2013-4-1
-	float y_distance;//内容--y轴方向上摄像机偏离机床基准点的距离，姓名--刘旋，时间--2013-4-1
-	float z_distance;//内容--z轴方向上摄像机偏离机床基准点的距离，姓名--刘旋，时间--2013-4-1
-	Vector3 front_position;//内容--正视图下摄像机偏离机床基准点的位置坐标，姓名--刘旋，时间--2013-4-1
-	Vector3 top_position;//内容--俯视图下摄像机偏离机床基准点的位置坐标，姓名--刘旋，时间--2013-4-1
-	Vector3 left_position;//内容--左视图下摄像机偏离机床基准点的位置坐标，姓名--刘旋，时间--2013-4-1
+	Vector3 current_position;
+	Vector3 targetFront_position;
+	Vector3 targetTop_position;
+	Vector3 targetLeft_position;
+	Vector3 current_angles;
+	Vector3 targetFront_angles;
+	Vector3 targetTop_angles;
+	Vector3 targetLeft_angles;
+	bool front_flag=false;
+	bool top_flag=false;
+	bool left_flag=false;
+	float currentView;
+	float targetView;
 	
 	
 	void Awake()
 	{
-		mainObject=GameObject.Find("zero");//内容--选取机床零点坐标作为机床的基准点，姓名--刘旋，时间--2013-4-1
+		//mainObject=GameObject.Find("GZT");//内容--选取机床零点坐标作为机床的基准点，姓名--刘旋，时间--2013-4-1
 		mainCamera=GameObject.Find("Main Camera");
-		z_distance=30f;
-		left_position=new Vector3(0f,1.2f,z_distance);
-		y_distance=45f;
-		top_position=new Vector3(0f,y_distance,-1.1f);
-		x_distance=30f;
-		front_position=new Vector3(-x_distance,1.2f,-0.5f);
+		targetFront_position=new Vector3(-30f,1.2f,-1.1f);
+		targetTop_position=new Vector3(0,45f,-1.1f);
+		targetLeft_position=new Vector3(0,1.2f,30f);
+		targetFront_angles=new Vector3(0,90f,0);
+		targetTop_angles=new Vector3(90f,0,0);
+		targetLeft_angles=new Vector3(0,180f,0);
+		targetView=6f;
+		
 		
 	}
 
@@ -35,36 +44,76 @@ public class ViewSelectModule : MonoBehaviour {
 	}
 	void OnGUI()
 	{
+		currentView=mainCamera.camera.fieldOfView;
+		string temp=currentView.ToString();
+		Debug.Log(temp);
 		if(GUI.Button(new Rect(10,190,100,40),"Front View"))
-			Front();
+		{
+			front_flag=true;
+			top_flag=false;
+			left_flag=false;
+			currentView=30f;
+		}
 		if(GUI.Button(new Rect(10,240,100,40),"Top View"))
-			Top();
+		{
+			front_flag=false;
+			top_flag=true;
+			left_flag=false;
+			currentView=30f;
+		}
 		if(GUI.Button(new Rect(10,290,100,40),"Left View"))
+		{
+			front_flag=false;
+			top_flag=false;
+			left_flag=true;
+			currentView=30f;
+		}
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+		if (front_flag)
+			Front();
+		if (top_flag)
+			Top();
+		if (left_flag)
 			Left();
 	}
 	void Front()
 	{
-		mainCamera.transform.position=mainObject.transform.position;
-		mainCamera.transform.rotation=mainObject.transform.rotation;//内容--使摄像机的位置和旋转信息与机床基准点保持一致，姓名--刘旋，时间--2013-4-1
-		mainCamera.transform.position+=front_position;
-		mainCamera.transform.Rotate(0f,90f,0f);//内容--摄像机相对于基准点进行偏离和旋转，姓名--刘旋，时间--2013-4-1
+		mainCamera.camera.transform.LookAt(new Vector3(0,1.2f,-1.1f));
+		current_position=mainCamera.transform.position;
+		current_position=Vector3.Lerp(current_position,targetFront_position,Time.deltaTime*5f);
+		mainCamera.transform.position=current_position;
+		current_angles=mainCamera.transform.eulerAngles;
+		current_angles=Vector3.Lerp(current_angles,targetFront_angles,Time.deltaTime*5f);
+		mainCamera.transform.eulerAngles=current_angles;
+		currentView=Mathf.LerpAngle(currentView,targetView,Time.deltaTime*5f);
+		mainCamera.camera.fieldOfView=currentView;
 	}
 	void Top()
 	{
-		mainCamera.transform.position=mainObject.transform.position;
-		mainCamera.transform.rotation=mainObject.transform.rotation;
-		mainCamera.transform.Rotate(90f,0f,0f);
-		mainCamera.transform.position+=top_position;
+		//mainCamera.camera.transform.LookAt(Vector3.zero);
+		current_position=mainCamera.transform.position;
+		current_position=Vector3.Lerp(current_position,targetTop_position,Time.deltaTime*5f);
+		mainCamera.transform.position=current_position;
+		current_angles=mainCamera.transform.eulerAngles;
+		current_angles=Vector3.Lerp(current_angles,targetTop_angles,Time.deltaTime*5f);
+		mainCamera.transform.eulerAngles=current_angles;
+		currentView=Mathf.LerpAngle(currentView,targetView,Time.deltaTime*5f);
+		mainCamera.camera.fieldOfView=currentView;
 	}
 	void Left()
 	{
-		mainCamera.transform.position=mainObject.transform.position;
-		mainCamera.transform.rotation=mainObject.transform.rotation;
-		mainCamera.transform.position+=left_position;
-		mainCamera.transform.Rotate(0f,180f,0);
+		mainCamera.camera.transform.LookAt(new Vector3(0,1.2f,-1.1f));
+		current_position=mainCamera.transform.position;
+		current_position=Vector3.Lerp(current_position,targetLeft_position,Time.deltaTime*5f);
+		mainCamera.transform.position=current_position;
+		current_angles=mainCamera.transform.eulerAngles;
+		current_angles=Vector3.Lerp(current_angles,targetLeft_angles,Time.deltaTime*5f);
+		mainCamera.transform.eulerAngles=current_angles;
+		currentView=Mathf.LerpAngle(currentView,targetView,Time.deltaTime*5f);
+		mainCamera.camera.fieldOfView=currentView;
 	}
-	// Update is called once per frame
-	void Update () {
 	
-	}
 }
